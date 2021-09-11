c7fec26fa5d061af7863f68e31d3ff4aa72a7ec0
Samuel Pereira <samuelpereira@gec.inatel.br>
2021-09-10 21:02:42 -0300

    implement calc

--- calc.js
+++ calc.js
@@ -1,7 +1,24 @@
 const args = process.argv.slice();
-console.log(process.argv);
-for (let index = 0; index < args.length; index++) {
-    if (isNaN(args[index])) {
-        console.log(`Arg[${index}] is ${args[index]}`);
+//console.log(process.argv);
+
+let op = "";
+
+if (args[2] == "-") {
+    console.log("menos");
+    op = "sub"
+} else if (args[2] == "+") {
+    console.log("mais");
+    op = "sum"
+}
+
+var total = Number(args[3]);
+
+for (let index = 4; index < args.length; index++) {
+    if (op == "sum") {
+        total += Number(args[index]);
+    } else if (op == "sub") {
+        total -= Number(args[index]);
     }
 }
+
+console.log("Resultado: " + total)
